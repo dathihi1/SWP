c8c3f6629450d470ba05005dd9c418b81124b926
DucDepTrai <daominhduc1105@gmail.com>
2025-10-21 19:54:48 +0700

    fix view

--- study1/src/main/java/com/badat/study1/controller/ViewController.java
+++ study1/src/main/java/com/badat/study1/controller/ViewController.java
@@ -22,13 +22,11 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
