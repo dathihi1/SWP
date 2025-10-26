77b4a79c16907e827d598cd799c8a00bf4f52881
DatHihi <dat2801zz@gmail.com>
2025-10-26 16:36:01 +0700

    fix merge

--- study1/src/main/java/com/badat/study1/configuration/WebMvcConfig.java
+++ study1/src/main/java/com/badat/study1/configuration/WebMvcConfig.java
@@ -1,9 +1,20 @@
 package com.badat.study1.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.ResourceResolver;
+import org.springframework.web.servlet.resource.ResourceUrlProvider;
+import org.springframework.web.servlet.resource.ResourceUrlProviderExposingInterceptor;
 
 @Configuration
-public class WebMvcConfig {
-    // ApiLoggingInterceptor has been removed and replaced with ApiCallLogFilter
-    // which is configured in the Spring Security filter chain
+public class WebMvcConfig implements WebMvcConfigurer {
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .resourceChain(false);
+    }
 }
